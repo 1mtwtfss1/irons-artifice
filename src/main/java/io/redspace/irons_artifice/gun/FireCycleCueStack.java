7682ad0ad3bf62726a578498a30b0055a3821e44
iron431 <34083081+iron431@users.noreply.github.com>
2026-08-16 09:01:59 -0500

    fix server/client audio double play

--- src/main/java/io/redspace/irons_artifice/gun/FireCycleCueStack.java
+++ src/main/java/io/redspace/irons_artifice/gun/FireCycleCueStack.java
@@ -49,9 +49,11 @@ public class FireCycleCueStack {
         while (cueIndex < cues.size() && percent >= cues.get(cueIndex).percent()) {
             var sound = cues.get(cueIndex).sound();
             var adjustedSound = new PlayableSound(sound.soundEventHolder(), sound.volume(), sound.minPitch() * pitchMultiplier, sound.maxPitch() * pitchMultiplier);
-            owner.level().playSound(owner, pos.x, pos.y, pos.z, adjustedSound.soundEventHolder().value(), source, adjustedSound.volume(), adjustedSound.samplePitch(owner.getRandom()));
-            if (owner instanceof ServerPlayer serverPlayer) {
-                PacketDistributor.sendToPlayer(serverPlayer, new ClientboundLocalSoundPacket(source, adjustedSound));
+            if (!owner.level().isClientSide()) {
+                owner.level().playSound(owner, pos.x, pos.y, pos.z, adjustedSound.soundEventHolder().value(), source, adjustedSound.volume(), adjustedSound.samplePitch(owner.getRandom()));
+                if (owner instanceof ServerPlayer serverPlayer) {
+                    PacketDistributor.sendToPlayer(serverPlayer, new ClientboundLocalSoundPacket(source, adjustedSound));
+                }
             }
             cueIndex++;
         }
